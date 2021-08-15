888630d9590865c3f3e13ab7bd32211bd82c7777
Kelvinthedrugger <890225kelvin@gmail.com>
2021-08-15 18:13:04 +0800

    fix several typo for readibility

--- evidential_deep_learning/losses/continuous.py
+++ evidential_deep_learning/losses/continuous.py
@@ -8,25 +8,31 @@ def MSE(y, y_, reduce=True):
     mse = tf.reduce_mean((y-y_)**2, axis=ax)
     return tf.reduce_mean(mse) if reduce else mse
 
+
 def RMSE(y, y_):
     rmse = tf.sqrt(tf.reduce_mean((y-y_)**2))
     return rmse
 
+
 def Gaussian_NLL(y, mu, sigma, reduce=True):
     ax = list(range(1, len(y.shape)))
 
-    logprob = -tf.math.log(sigma) - 0.5*tf.math.log(2*np.pi) - 0.5*((y-mu)/sigma)**2
-    loss = tf.reduce_mean(-logprob, axis=ax)
+    neglogprob = tf.math.log(sigma) + 0.5 * \
+        tf.math.log(2*np.pi) + 0.5*((y-mu)/sigma)**2
+    loss = tf.reduce_mean(neglogprob, axis=ax)
     return tf.reduce_mean(loss) if reduce else loss
+
 
 def Gaussian_NLL_logvar(y, mu, logvar, reduce=True):
     ax = list(range(1, len(y.shape)))
 
     log_liklihood = 0.5 * (
-        -tf.exp(-logvar)*(mu-y)**2 - tf.math.log(2*tf.constant(np.pi, dtype=logvar.dtype)) - logvar
+        -tf.exp(-logvar)*(mu-y)**2 -
+        tf.math.log(2 * tf.constant(np.pi, dtype=logvar.dtype)) - logvar
     )
     loss = tf.reduce_mean(-log_liklihood, axis=ax)
     return tf.reduce_mean(loss) if reduce else loss
+
 
 def NIG_NLL(y, gamma, v, alpha, beta, reduce=True):
     twoBlambda = 2*beta*(1+v)
@@ -39,6 +45,7 @@ def NIG_NLL(y, gamma, v, alpha, beta, reduce=True):
 
     return tf.reduce_mean(nll) if reduce else nll
 
+
 def KL_NIG(mu1, v1, a1, b1, mu2, v2, a2, b2):
     KL = 0.5*(a1-1)/b1 * (v2*tf.square(mu2-mu1))  \
         + 0.5*v2/v1  \
@@ -48,6 +55,7 @@ def KL_NIG(mu1, v1, a1, b1, mu2, v2, a2, b2):
         + (a1 - a2)*tf.math.digamma(a1)  \
         - (b1 - b2)*a1/b1
     return KL
+
 
 def NIG_Reg(y, gamma, v, alpha, beta, omega=0.01, reduce=True, kl=False):
     # error = tf.stop_gradient(tf.abs(y-gamma))
@@ -61,6 +69,7 @@ def NIG_Reg(y, gamma, v, alpha, beta, omega=0.01, reduce=True, kl=False):
         reg = error*evi
 
     return tf.reduce_mean(reg) if reduce else reg
+
 
 def EvidentialRegression(y_true, evidential_output, coeff=1.0):
     gamma, v, alpha, beta = tf.split(evidential_output, 4, axis=-1)
